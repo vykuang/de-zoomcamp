1803bbaa7e8f255fc6499b37772a31320d0e92d7
victor kuang <vykuang92@gmail.com>
2023-02-14 15:28:15 +0000

    homework wip; need to deduplicate fact_trips

--- dbt-models/models/staging/stg_fhv_taxi_trips.sql
+++ dbt-models/models/staging/stg_fhv_taxi_trips.sql
@@ -1,19 +1,22 @@
 {{ config(materialized="view") }}
 
--- select *
--- from {{ source('staging', 'fhv_taxi_trips') }}
--- limit 100
--- with
--- tripdata as (
--- select *, row_number() over (partition by pickup_datetime) as row_num
--- from {{ source("staging", "fhv_taxi_trips") }}
--- where pickup_datetime is not null
--- )
+
+with fhv_trips as (
+    select 
+        *, 
+        row_number() over (partition by pickup_datetime) as row_num
+    from {{ source("staging", "fhv_taxi_trips") }}
+    where 
+        pickup_datetime is not null and
+        dropoff_datetime is not null and
+        PULocationID is not null and
+        DOLocationID is not null
+)
 select
     -- identifiers
-    {{ dbt_utils.surrogate_key(["PUlocationID", "pickup_datetime"]) }} as tripid,
-    cast(pulocationid as string) as  pickup_locationid,
-    cast(dolocationid as string) as dropoff_locationid,
+    {{ dbt_utils.surrogate_key(["PULocationID", "pickup_datetime", "dropoff_datetime"]) }} as tripid,
+    cast(PULocationID as string) as pickup_locationid,
+    cast(DOLocationID as string) as dropoff_locationid,
 
     -- timestamps
     cast(pickup_datetime as timestamp) as pickup_datetime,
@@ -25,8 +28,7 @@ select
     -- base number info
     cast(dispatching_base_num as string) as dispatching_base_num,
     cast(affiliated_base_number as string) as affiliated_base_num
--- from tripdata
-from {{ source("staging", "fhv_taxi_trips") }}
--- where row_num = 1
+from fhv_trips
+where row_num = 1
 -- dbt build --m model.sql --var 'is_test_run:false'
 {% if var("is_test_run", default=True) %} limit 100 {% endif %}
--- w4-analytics-eng/hw.md
+++ w4-analytics-eng/hw.md
@@ -1,9 +1,17 @@
 # Week 4 - Analytics Engineering - Homework
 
 1. num rows in `fact_trips`: 115,716,952
+  - need to deduplicate. Should be around 63M
 1. distribution between service types (green vs yellow) between 2019 and 2020?
-  - use google data studio or metabase
+  - use google data studio (now known as looker) or metabase
 1. num rows in `stg_fhv_tripdata`: 43261276
+  - Actually, there are a lot of duplicates for `tripid` even when I include `dropoff_datetime` as part of the surrogate key
+  - `affiliated_base_num` is the culprit
+  - for the exact same trip, there are many duplicates with different `affiliated_base_num`
+  - may be an idiosyncrasy of their record keeping, but I think they're the same trip?
+  - one example I saw had 39 identical pu/do ID, and pu/do datetimes
+  - use the `row_number() over partition` trick?
+  - dropped to 14633830 after deduplication
 1. Create a core model for the stg_fhv_tripdata joining with dim_zones. 
 Similar to what we've done in fact_trips, keep only records with known pickup and dropoff locations entries 
 for pickup and dropoff locations
